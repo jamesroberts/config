ae462f5b687c3420319e449278b7e0729d72709d
James Roberts <jamesroberts.dev@gmail.com>
2023-07-22 17:10:21 +0200

    Updated setup

---
+++ setup.sh
@@ -0,0 +1,39 @@
+cd $HOME
+
+function config {
+    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+add_to_config_file() {
+    echo 'alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"' >> $1
+    echo 'source "$HOME/.shell_rc"' >> $1
+}
+
+BASHRC=$HOME/.bashrc
+if test -f "$BASHRC"; then
+    echo "Appending config to $BASHRC"
+    add_to_config_file $BASHRC
+fi
+
+ZSHRC=$HOME/.zshrc
+if test -f "$ZSHRC"; then
+    echo "Appending config to $ZSHRC"
+    add_to_config_file $ZSHRC
+fi
+
+echo ".cfg" >> .gitignore
+
+git clone --bare git@github.com:jamesroberts/config.git $HOME/.cfg
+
+mkdir -p .config-backup
+
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing config files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+
+config checkout
+config config --local status.showUntrackedFiles no
