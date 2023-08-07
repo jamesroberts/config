09ea23c59e0677b0e9aea158aa999e3006e9b70e
James Roberts <jamesroberts.dev@gmail.com>
2023-08-07 12:53:19 +0000

    Added nvim autocmd

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+
+-- Remove trailing whitespace on save
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    group = vim.api.nvim_create_augroup("RemoveTrailingWhitespace", { clear = true }),
+    command = '%s/\\s\\+$//e'
+})
