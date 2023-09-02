071cff2f6c18e773c5498cbec9182fe55f10b721
James Roberts <jamesroberts.dev@gmail.com>
2023-09-02 13:51:16 +0000

    Fix autocomplete and disabled mini.pairs

---
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -0,0 +1,90 @@
+return {
+    {
+        "hrsh7th/nvim-cmp",
+        version = false, -- last release is way too old
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "saadparwaiz1/cmp_luasnip",
+        },
+        opts = function()
+            vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+            local defaults = require("cmp.config.default")()
+
+            local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
+
+            return {
+                completion = {
+                    completeopt = "menu,menuone,noinsert,noselect,preview",
+                },
+                snippet = {
+                    expand = function(args)
+                    require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    -- ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+                    -- ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-u>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ["<S-CR>"] = cmp.mapping.confirm({
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = false,
+                    }),
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        elseif has_words_before() then
+                            cmp.complete()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+                formatting = {
+                    format = function(_, item)
+                    local icons = require("lazyvim.config").icons.kinds
+                    if icons[item.kind] then
+                        item.kind = icons[item.kind] .. item.kind
+                    end
+                    return item
+                    end,
+                },
+                experimental = {
+                    ghost_text = {
+                    hl_group = "CmpGhostText",
+                    },
+                },
+                sorting = defaults.sorting,
+            }
+        end
+    }
+}
